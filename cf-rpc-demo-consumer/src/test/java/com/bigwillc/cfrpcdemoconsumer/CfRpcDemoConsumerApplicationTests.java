979a8816deb47fef55b0e2a471cc2faee76dae0f
bigwillc <chenfenghuang@MacBook-Pro.local>
2024-03-27 20:14:17 +0800

    修改maven配置，打包

--- cf-rpc-demo-consumer/src/test/java/com/bigwillc/cfrpcdemoconsumer/CfRpcDemoConsumerApplicationTests.java
+++ cf-rpc-demo-consumer/src/test/java/com/bigwillc/cfrpcdemoconsumer/CfRpcDemoConsumerApplicationTests.java
@@ -1,13 +1,33 @@
 package com.bigwillc.cfrpcdemoconsumer;
 
+import com.bigwillc.cfrpcdemoprovider.CfRpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CfRpcDemoConsumerApplicationTests {
 
+	static ApplicationContext context;
+
+	@BeforeAll
+	static void init() {
+		context = SpringApplication.run(CfRpcDemoProviderApplication.class, "--server.port=8081");
+	}
+
 	@Test
 	void contextLoads() {
+		System.out.println(" ===> aaa ...");
 	}
+
+	@AfterAll
+	static void destroy() {
+		SpringApplication.exit(context, () -> 1);
+	}
+
+
 
 }
